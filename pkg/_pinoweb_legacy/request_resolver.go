1e59e2f760c116a5ab6bed1b59743332b408e4bf
Manuel Odendahl <wesen@ruinwesen.com>
2026-07-06 16:48:38 -0400

    Remove legacy pinoweb quarantine

--- pkg/_pinoweb_legacy/request_resolver.go
+++
@@ -1,9 +0,0 @@
-package pinoweb
-
-import profilechat "github.com/go-go-golems/go-go-os-chat/pkg/profilechat"
-
-type StrictRequestResolver = profilechat.StrictRequestResolver
-
-func NewStrictRequestResolver(runtimeKey string) *StrictRequestResolver {
-	return profilechat.NewStrictRequestResolver(runtimeKey)
-}
